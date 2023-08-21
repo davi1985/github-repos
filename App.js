ac9fcf3a217082a2a47c7516c82f96b78ab10327
Davi Silva <davi.psilva@midway.com.br>
2023-08-21 11:19:44 -0300

    initial files

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
+import './src/config/ReactotronConfig';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
+import { registerRootComponent } from 'expo';
+
+import { name as appName } from './app.json';
+import Root from './src';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+
+      <Root />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+if (Platform.OS == 'android') {
+  registerRootComponent(App);
+} else {
+  AppRegistry.registerComponent(appName, () => App);
+}
---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,7 @@
+import { View, Text } from 'react-native';
+
+export const Welcome = () => (
+  <View>
+    <Text>Welcome page</Text>
+  </View>
+);
